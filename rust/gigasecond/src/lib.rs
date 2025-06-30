4652b89a4ee366c2791e6df24a04ab5163daa664
Nolle <everaert.nolle@gmail.com>
2025-06-30 13:53:40 +0200

    Added new exercise + finished gigaseconds

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,6 +1,8 @@
+use time::Duration;
 use time::PrimitiveDateTime as DateTime;
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    todo!("What time is a gigasecond later than {start}");
+    let offset: i64 = 1_000_000_000;
+    return start + Duration::seconds(offset);
 }
